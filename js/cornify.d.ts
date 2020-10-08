38040d513edf6c867eba11b120c8d48754fe5a87
Wes Bos <wesbos@gmail.com>
2020-10-08 13:15:00 -0400

    add typescript typings for cornify

---
+++ js/cornify.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+interface CornifyOptions {
+  younicorns?: string;
+  y?: string
+}
+
+declare function cornify_add(options: CornifyOptions): void;
